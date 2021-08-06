d1046955ec4d078fc3da159cdb25eb62ffdd7a65
Allanetizen <allankosgei747@gmail.com>
2021-08-06 16:16:50 +0300

    finalized webview

---
+++ lib/audio.dart
@@ -0,0 +1,51 @@
+// child: Column(
+// children: [
+// Row(
+// children: [
+// Text('Check what you do below:\n\n'),
+// ],
+// ),
+// Row(
+// children: [
+// Checkbox(
+// value: _checkbox,
+// onChanged: (value) {
+// setState(() {
+// _checkbox = !_checkbox;
+// });
+// },
+// ),
+// Text(
+// 'Take Coffee before sleeping',
+// //style: TextStyle(color : Colors.white),
+// ),
+// ],
+// ),
+// // ignore: deprecated_member_use
+//
+// CheckboxListTile(
+// controlAffinity: ListTileControlAffinity.leading,
+// title: Text('Drink Alcohol'),
+// value: _checkboxListTile,
+// onChanged: (value) {
+// setState(() {
+// _checkboxListTile = !_checkboxListTile;
+// });
+// },
+// ),
+// Row(
+// children: [
+// // ignore: deprecated_member_use
+// RaisedButton(
+// color: Colors.orangeAccent,
+// onPressed: () {
+// Navigator.pop(context);
+// },
+// child: Text('Go back'),
+// ),
+// ],
+// ),
+// ],
+//
+// // ignore: deprecated_member_use
+// ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:webview_flutter/webview_flutter.dart';
+//flutter run --no-sound-null-safety
+
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:audioplayers/audio_cache.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,7 +48,7 @@ class SplashScreenState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.deepPurple,
+      color: Colors.lightBlueAccent,
       child: CircleAvatar(
         backgroundImage: AssetImage('assets/images/insomnapp.png'),
         radius: 70.0,
@@ -55,22 +69,24 @@ class HomeScreen extends StatelessWidget {
         ),
         // ignore: deprecated_member_use
         body: Center(
+
           // ignore: deprecated_member_use
           child: Row(
+
             children: [
               // ignore: deprecated_member_use
 
               // ignore: deprecated_member_use
               Column(
-                children: <Widget> [
+                children: <Widget>[
                   // ignore: deprecated_member_use
                   RaisedButton(
                     child: Text('Self Diagnosis'),
-                    color: Colors.orangeAccent,
+                    color: Colors.lightBlueAccent,
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => SecondRoute()),
+                        MaterialPageRoute(builder: (context) => Diagnose()),
                       );
                     },
                   ),
@@ -78,7 +94,7 @@ class HomeScreen extends StatelessWidget {
                   // ignore: deprecated_member_use
                   RaisedButton(
                     child: Text('Contact Psychiatrist'),
-                    color: Colors.orangeAccent,
+                    color: Colors.lightBlueAccent,
                     onPressed: () {
                       Navigator.push(
                         context,
@@ -89,11 +105,11 @@ class HomeScreen extends StatelessWidget {
                   // ignore: deprecated_member_use
                   RaisedButton(
                     child: Text('Play White noise'),
-                    color: Colors.orangeAccent,
+                    color: Colors.lightBlue,
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Contact()),
+                        MaterialPageRoute(builder: (context) => MusicApp()),
                       );
                     },
                   ),
@@ -108,82 +124,27 @@ class HomeScreen extends StatelessWidget {
         ));
   }
 }
-
-class SecondRoute extends StatefulWidget {
-  @override
-  _SecondRouteState createState() => _SecondRouteState();
-}
-
-class _SecondRouteState extends State<SecondRoute> {
-  bool _checkbox = false;
-  bool _checkboxListTile = false;
+class Diagnose extends StatelessWidget {
+  const Diagnose({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        //backgroundColor: Colors.grey[900],
+    return Container(
+      child: _buildWebView(),
 
-        appBar: AppBar(
-          title: Text("Self Diagnosis"),
-          //backgroundColor: Colors.grey[850],
-          elevation: 0.0,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Text('Check what you do below:\n\n'),
-                ],
-              ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: _checkbox,
-                    onChanged: (value) {
-                      setState(() {
-                        _checkbox = !_checkbox;
-                      });
-                    },
-                  ),
-                  Text(
-                    'Take Coffee before sleeping',
-                    //style: TextStyle(color : Colors.white),
-                  ),
-                ],
-              ),
-              // ignore: deprecated_member_use
+    );
+  }
 
-              CheckboxListTile(
-                controlAffinity: ListTileControlAffinity.leading,
-                title: Text('Drink Alcohol'),
-                value: _checkboxListTile,
-                onChanged: (value) {
-                  setState(() {
-                    _checkboxListTile = !_checkboxListTile;
-                  });
-                },
-              ),
-              Row(
-                children: [
-                  // ignore: deprecated_member_use
-                  RaisedButton(
-                    color: Colors.orangeAccent,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Go back'),
-                  ),
-                ],
-              ),
-            ],
+  Widget _buildWebView() {
+    return WebView(
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: 'https://insomnapp-ml.herokuapp.com/',
 
-            // ignore: deprecated_member_use
-          ),
-        ));
+    );
+
   }
 }
-
+//--------
 class Contact extends StatefulWidget {
   const Contact({Key? key}) : super(key: key);
 
@@ -242,6 +203,249 @@ class _ContactState extends State<Contact> {
             child: Text('SMS'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MusicApp extends StatefulWidget {
+  @override
+  _MusicAppState createState() => _MusicAppState();
+}
+
+class _MusicAppState extends State<MusicApp> {
+  //we will need some variables
+  bool playing = false; // at the beginning we are not playing any song
+  IconData playBtn = Icons.play_arrow; // the main state of the play button icon
+
+  //Now let's start by creating our music player
+  //first let's declare some object
+  late AudioPlayer _player;
+  late AudioCache cache;
+
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+
+  //we will create a custom slider
+
+  Widget slider() {
+    return Container(
+      width: 300.0,
+      child: Slider.adaptive(
+          activeColor: Colors.blue[800],
+          inactiveColor: Colors.grey[350],
+          value: position.inSeconds.toDouble(),
+          max: musicLength.inSeconds.toDouble(),
+          onChanged: (value) {
+            seekToSec(value.toInt());
+          }),
+    );
+  }
+
+  //let's create the seek function that will allow us to go to a certain position of the music
+  void seekToSec(int sec) {
+    Duration newPos = Duration(seconds: sec);
+    _player.seek(newPos);
+  }
+
+  //Now let's initialize our player
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _player = AudioPlayer();
+    cache = AudioCache(fixedPlayer: _player);
+
+    //now let's handle the audioplayer time
+
+    //this function will allow you to get the music duration
+    // ignore: deprecated_member_use
+    _player.durationHandler = (d) {
+      setState(() {
+        musicLength = d;
+      });
+    };
+
+    //this function will allow us to move the cursor of the slider while we are playing the song
+    // ignore: deprecated_member_use
+    _player.positionHandler = (p) {
+      setState(() {
+        position = p;
+      });
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //let's start by creating the main UI of the app
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blue,
+                Colors.blue,
+              ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 48.0,
+          ),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Let's add some text title
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Music Beats",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 38.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "ListenCal,img Sounds",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                //Let's add the music cover
+                Center(
+                  child: Container(
+                    width: 280.0,
+                    height: 280.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/dandelion.jpg"),
+                        )),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 18.0,
+                ),
+                Center(
+                  child: Text(
+                    "Nature Sounds",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        //Let's start by adding the controller
+                        //let's add the time indicator text
+
+                        Container(
+                          width: 500.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "${position.inMinutes}:${position.inSeconds.remainder(60)}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                              slider(),
+                              Text(
+                                "${musicLength.inMinutes}:${musicLength.inSeconds.remainder(60)}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              iconSize: 45.0,
+                              color: Colors.blue,
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.skip_previous,
+                              ),
+                            ),
+                            IconButton(
+                              iconSize: 62.0,
+                              color: Colors.blue[800],
+                              onPressed: () {
+                                //here we will add the functionality of the play button
+                                if (!playing) {
+                                  //now let's play the song
+                                  cache.play("Nature Sounds.mp3");
+                                  setState(() {
+                                    playBtn = Icons.pause;
+                                    playing = true;
+                                  });
+                                } else {
+                                  _player.pause();
+                                  setState(() {
+                                    playBtn = Icons.play_arrow;
+                                    playing = false;
+                                  });
+                                }
+                              },
+                              icon: Icon(
+                                playBtn,
+                              ),
+                            ),
+                            IconButton(
+                              iconSize: 45.0,
+                              color: Colors.blue,
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.skip_next,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
